2870e4e7e0c168ae17a02388505db4b8dc61cec4
b-sharman <byron.n.sharman@gmail.com>
2024-05-19 03:42:54 -0600

    initial commit

---
+++ home.nix
@@ -0,0 +1,127 @@
+{ config, pkgs, ... }:
+
+{
+  # Home Manager needs a bit of information about you and the paths it should
+  # manage.
+  home.username = "byron";
+  home.homeDirectory = "/home/byron";
+
+  # This value determines the Home Manager release that your configuration is
+  # compatible with. This helps avoid breakage when a new Home Manager release
+  # introduces backwards incompatible changes.
+  #
+  # You should not change this value, even if you update Home Manager. If you do
+  # want to update the value, then make sure to first check the Home Manager
+  # release notes.
+  home.stateVersion = "23.11"; # Please read the comment before changing.
+
+  # The home.packages option allows you to install Nix packages into your
+  # environment.
+  home.packages = with pkgs; [
+    # # Adds the 'hello' command to your environment. It prints a friendly
+    # # "Hello, world!" when run.
+    # pkgs.hello
+
+    # # It is sometimes useful to fine-tune packages, for example, by applying
+    # # overrides. You can do that directly here, just don't forget the
+    # # parentheses. Maybe you want to install Nerd Fonts with a limited number of
+    # # fonts?
+    # (pkgs.nerdfonts.override { fonts = [ "FantasqueSansMono" ]; })
+
+    # # You can also create simple shell scripts directly inside your
+    # # configuration. For example, this adds a command 'my-hello' to your
+    # # environment:
+    # (pkgs.writeShellScriptBin "my-hello" ''
+    #   echo "Hello, ${config.home.username}!"
+    # '')
+  ];
+
+  # Home Manager is pretty good at managing dotfiles. The primary way to manage
+  # plain files is through 'home.file'.
+  home.file = {
+    # # Building this configuration will create a copy of 'dotfiles/screenrc' in
+    # # the Nix store. Activating the configuration will then make '~/.screenrc' a
+    # # symlink to the Nix store copy.
+    # ".screenrc".source = dotfiles/screenrc;
+
+    # # You can also set the file content immediately.
+    # ".gradle/gradle.properties".text = ''
+    #   org.gradle.console=verbose
+    #   org.gradle.daemon.idletimeout=3600000
+    # '';
+  };
+
+  # Home Manager can also manage your environment variables through
+  # 'home.sessionVariables'. If you don't want to manage your shell through Home
+  # Manager then you have to manually source 'hm-session-vars.sh' located at
+  # either
+  #
+  #  ~/.nix-profile/etc/profile.d/hm-session-vars.sh
+  #
+  # or
+  #
+  #  /etc/profiles/per-user/byron/etc/profile.d/hm-session-vars.sh
+  #
+  home.sessionVariables = {
+    EDITOR = "nvim";
+  };
+
+  programs = {
+    # Let Home Manager install and manage itself.
+    home-manager.enable = true;
+
+    git = {
+      enable = true;
+      extraConfig = {
+        init.defaultBranch = "main";
+      };
+      userEmail = "byron.n.sharman@gmail.com";
+      userName = "b-sharman";
+    };
+
+    neovim = {
+      enable = true;
+      extraLuaConfig = builtins.readFile neovim/init.lua;
+      defaultEditor = true;
+      plugins = with pkgs.vimPlugins; [
+        {
+          type = "lua";
+          plugin = nvim-treesitter.withAllGrammars;
+          config = ''
+            require('nvim-treesitter.configs').setup({
+              highlight = {
+                enable = true,
+                additional_vim_regex_highlighting = false,
+               },
+              indent = { enable = true },
+            })
+          '';
+        }
+        {
+          type = "lua";
+          plugin = plenary-nvim;
+        }
+        {
+          type = "lua";
+          plugin = telescope-nvim;
+          config = ''
+            local builtin = require('telescope.builtin')
+            vim.keymap.set('n', '<leader>f', builtin.find_files, {})
+          '';
+        }
+      ];
+    };
+
+    zsh = {
+      autosuggestion.enable = true;
+      defaultKeymap = "viins";
+      enable = true;
+      enableCompletion = true;
+      initExtra = builtins.readFile zsh/init.sh;
+      shellAliases = {
+        grep = "grep --color=auto";
+      };
+      syntaxHighlighting.enable = true;
+    };
+  };
+}
---
+++ neovim/init.lua
@@ -0,0 +1,17 @@
+vim.cmd("colorscheme retrobox")
+
+vim.opt.hlsearch = false
+
+-- tab stuff
+vim.opt.expandtab = true
+vim.opt.shiftwidth = 2
+vim.opt.softtabstop = -1
+vim.opt.smarttab = true
+
+-- highlight yanked text for 200ms
+vim.cmd[[
+  augroup highlight_yank
+  autocmd!
+  au TextYankPost * silent! lua vim.highlight.on_yank({higroup="IncSearch", timeout=200})
+  augroup END
+]]
