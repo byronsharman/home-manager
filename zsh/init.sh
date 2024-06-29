3bdb7aaed6909365cef2d0dab9869432554e199c
b-sharman <byron.n.sharman@gmail.com>
2024-06-28 18:19:12 -0600

    jmp: tweaks for snowmane capability

--- zsh/init.sh
+++ zsh/init.sh
@@ -1,7 +1,7 @@
 # cd on steroids, made by Grant & Lukas
 function jmp {
   # remove the -H from fd to omit hidden files
-  cd "$(cat <(fd -H --base-directory=$HOME -E .cache -E .config/discord -E .config/google-chrome --type d -c never | awk '{print "~/"$0}' | sort) <(echo "~") | fzf $([ -z "$1" ] || echo "-q$1") --tac | sed "s#~#$HOME#g")"
+  cd "$(cat <(fd --base-directory=$HOME -E archives/coronado_archives -E venv --type d -c never | awk '{print "~/"$0}' | sort) <(echo "~") | fzf $([ -z "$1" ] || echo "-q$1") --tac | sed "s#~#$HOME#g")"
 }
 
 bindkey '^ ' autosuggest-accept
