b6272b255d83871143e581dff7ab7f46457e8bd7
b-sharman <byron.n.sharman@gmail.com>
2024-07-18 10:20:01 +1200

    nvim: try enabling incremental selection

--- home.nix
+++ home.nix
@@ -101,6 +101,15 @@
                 enable = true,
                 additional_vim_regex_highlighting = false,
                },
+               incremental_selection = {
+                 enable = true,
+                 keymaps = {
+                   init_selection = '<CR>',
+                   scope_incremental = '<CR>',
+                   node_incremental = '<TAB>',
+                   node_decremental = '<S-TAB>',
+                 },
+               },
               indent = { enable = true },
             })
           '';
