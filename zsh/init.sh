efd463f5a75e7bab56f323f14bb61b5566d5d513
b-sharman <byron.n.sharman@gmail.com>
2024-07-31 10:59:14 +1200

    jmp: optimize by excluding AUR stuff

--- zsh/init.sh
+++ zsh/init.sh
@@ -1,7 +1,7 @@
 # cd on steroids, made by Grant & Lukas
 function jmp {
   # remove the -H from fd to omit hidden files
-  cd "$(cat <(fd --base-directory=$HOME -E archives/coronado_archives -E venv --type d -c never | awk '{print "~/"$0}' | sort) <(echo "~") | fzf $([ -z "$1" ] || echo "-q$1") --tac | sed "s#~#$HOME#g")"
+  cd "$(cat <(fd --base-directory=$HOME -E archives/coronado_archives -E 'programs/aur/*' -E venv --type d -c never | awk '{print "~/"$0}' | sort) <(echo "~") | fzf $([ -z "$1" ] || echo "-q$1") --tac | sed "s#~#$HOME#g")"
 }
 
 bindkey '^ ' autosuggest-accept
