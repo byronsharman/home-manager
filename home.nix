f6a88e923f9dbcf9be4bc924bd0dc48e84011d8a
b-sharman <byron.n.sharman@gmail.com>
2024-08-02 00:14:40 +1200

    nvim: add harper-ls

--- home.nix
+++ home.nix
@@ -98,6 +98,27 @@
           config = ''
             require('lspconfig')
             require('lspconfig').svelte.setup{}
+            require('lspconfig').harper_ls.setup {
+              settings = {
+                ["harper-ls"] = {
+                  linters = {
+                    spell_check = false,
+                    spelled_numbers = false,
+                    an_a = true,
+                    sentence_capitalization = false,
+                    unclosed_quotes = true,
+                    wrong_quotes = false,
+                    long_sentences = false,
+                    repeated_words = true,
+                    spaces = true,
+                    matcher = true,
+                    correct_number_suffix = true,
+                    number_suffix_capitalization = true,
+                    multiple_sequential_pronouns = true
+                  }
+                }
+              },
+            }
           '';
         }
         {
