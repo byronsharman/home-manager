e683e2cc83fab5eeb540297cdf0dfbbfde5690e8
b-sharman <byron.n.sharman@gmail.com>
2025-03-29 22:32:29 -0600

    I no longer have a random virtual environment in my home directory

--- zsh/init.sh
+++ zsh/init.sh
@@ -3,7 +3,7 @@ export PS1='[%n@%M %1~]$ '
 # cd on steroids, made by Grant & Lukas
 function jmp {
   # remove the -H from fd to omit hidden files
-  cd "$(cat <(fd --base-directory=$HOME -E archives/coronado_archives -E 'programs/aur/*' -E venv --type d -c never | awk '{print "~/"$0}' | sort) <(echo "~") | fzf $([ -z "$1" ] || echo "-q$1") --tac | sed "s#~#$HOME#g")"
+  cd "$(cat <(fd --base-directory=$HOME -E archives/coronado_archives -E 'programs/aur/*' --type d -c never | awk '{print "~/"$0}' | sort) <(echo "~") | fzf $([ -z "$1" ] || echo "-q$1") --tac | sed "s#~#$HOME#g")"
 }
 
 bindkey '^ ' autosuggest-accept
