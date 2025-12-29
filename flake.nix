49358e068d0ff7c6cfbb7955955340f61f3d69ce
byronsharman <byron.n.sharman@gmail.com>
2025-12-29 09:53:16 -0700

    add flake.nix from template
    This was created with the command:
    
    nix run home-manager/master -- init --switch
    
    I think there are other commands, too, but it's probably best to just
    follow one tutorial

---
+++ flake.nix
@@ -0,0 +1,35 @@
+# NOTE TO FUTURE SELF: if running on NixOS, I'll want to discard this file and
+# start over. This file is for running home-manager in a standalone nix
+# installation. When on NixOS, home-manager functions best as a NixOS module.
+
+{
+  description = "Home Manager configuration of byron";
+
+  inputs = {
+    # Specify the source of Home Manager and Nixpkgs.
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    home-manager = {
+      url = "github:nix-community/home-manager";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+  };
+
+  outputs =
+    { nixpkgs, home-manager, ... }:
+    let
+      system = "x86_64-linux";
+      pkgs = nixpkgs.legacyPackages.${system};
+    in
+    {
+      homeConfigurations."byron" = home-manager.lib.homeManagerConfiguration {
+        inherit pkgs;
+
+        # Specify your home configuration modules here, for example,
+        # the path to your home.nix.
+        modules = [ ./home.nix ];
+
+        # Optionally use extraSpecialArgs
+        # to pass through arguments to home.nix
+      };
+    };
+}
